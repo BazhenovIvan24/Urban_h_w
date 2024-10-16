c302729ba3bc6b704a839cab50fabd762e422b0c
Ivan <bazhenovivan24@mail.ru>
2024-10-16 21:30:40 +0300

    Different way to complete exercise

--- Variable_1_0.py
+++ Variable_1_0.py
@@ -10,3 +10,8 @@ time_for_one_course = My_result.hour / My_result.quantity_hw
 print('Курс: ', name_of_course,  ', всего задач: ', My_result.quantity_hw, ', затрачено часов: ', My_result.hour, ', среднее время выполнения', time_for_one_course, 'часа')
 
 
+quantity_hw = 12
+hour = 1.5
+name_of_course = 'Python'
+time_for_one_course = hour / quantity_hw
+print ('Курс: ', name_of_course,  ', всего задач: ', quantity_hw, ', затрачено часов: ', hour, ', среднее время выполнения', time_for_one_course, 'часа')
