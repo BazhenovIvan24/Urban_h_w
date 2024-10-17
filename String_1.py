133a80371f94c170ceead86c49b5a140a1637e2d
Ivan <bazhenovivan24@mail.ru>
2024-10-17 07:09:51 +0300

    new_hw

---
+++ String_1.py
@@ -0,0 +1,9 @@
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+
+length_ex = len(example)
+middle_w = int(length_ex / 2)
+print(example[middle_w:])
+print(example[::-1])
+print(example[1:length_ex:2])
