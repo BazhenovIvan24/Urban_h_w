768729d8c5b63a10118fe3dc5f81f043c87a88d0
Ivan <bazhenovivan24@mail.ru>
2024-12-02 20:45:47 +0300

    module_3_2 create

--- module_3_2.py
+++ module_3_2.py
@@ -1,25 +1,55 @@
-def send_email(message, recipient = "university.help@gmail.com"):
-    test_recipient = tuple(recipient)
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    test_sender = tuple(sender)
     test1 = True
-    test2 = True
-    for char_i in test_recipient:
+    test2_1 = True
+    test2_2 = True
+    test2_3 = True
+    for char_i in test_sender:
         test1 = False
         if char_i == '@':
             test1 = True
             break
     if test1 == False:
-        print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
 
-    length_mail = len(test_recipient)
-    if test_recipient[length_mail-1] == 'm' or test_recipient[length_mail-1] == 'u' or test_recipient[length_mail-1] == 't':
-        test2 = True
+    length_mail = len(test_sender)
+    if test_sender[length_mail-1] == 'm' and test_sender[length_mail-2] == 'o' and test_sender[length_mail-3] == 'c' and test_sender[length_mail-4] == '.':
+        test2_1 = True
     else:
-        test2 = False
+        test2_1 = False
+    if test_sender[length_mail-1] == 'u' and test_sender[length_mail-2] == 'r' and test_sender[length_mail-3] == '.':
+        test2_2 = True
+    else:
+        test2_2 = False
+    if test_sender[length_mail-1] == 't' and test_sender[length_mail-2] == 'e' and test_sender[length_mail-3] == 'n' and test_sender[length_mail-4] == '.':
+        test2_3 = True
+    else:
+        test2_3 = False
 
-    if test2 == False:
-        print('Невозможно отправить письмо с адреса <sender> на адрес <recipient>')
 
 
-    return 0
+    if test2_1 == False and test2_2 == False and test2_3 == False:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        return
 
-send_email(1, '@1u' )
+    if sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+        return
+
+    if sender == 'university.help@gmail.com':
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+
+    if sender != 'university.help@gmail.com':
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+
+
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
