e0c4699ae1c1248c1b3bfd8b240f579e558092df
Ivan <vpk@vpk.npomash.ru>
2024-12-11 17:21:04 +0300

    change 2_2

--- module_2_2.py
+++ module_2_2.py
@@ -2,9 +2,11 @@ first = input()
 second = input()
 third = input()
 
-if (first == second) or (second == third) or (first == third):
-    print(2)
-elif first == second == third:
+if first == second == third:
     print(3)
+elif (first == second) or (second == third) or (first == third):
+    print(2)
 else:
     print(0)
+    
+    
