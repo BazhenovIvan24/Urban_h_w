2fd6504863bd74240a2ef5f69b69cf1899695b2e
Ivan <vpk@vpk.npomash.ru>
2024-12-11 17:39:12 +0300

    change 2_3

--- module_2_3.py
+++ module_2_3.py
@@ -1,15 +1,17 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 k = 0
-var1 = my_list[0]
-while var1 >= 0:
-    if (k + 1) > len(my_list):
-        break
+
+while k <= (len(my_list)-1):
     var1 = my_list[k]
-    k += 1
+    
     if var1 > 0:
         print(var1)
     elif var1 == 0:
+        k += 1
         continue
     else:
         break
+    k += 1
+	
+	
 
