aa3be11e0054a9f477d1b5e67e79cfeff72cb22a
Ivan <bazhenovivan24@mail.ru>
2024-12-12 23:16:11 +0300

    change files 12_12

---
+++ module_2_hard.py
@@ -0,0 +1,33 @@
+n = int(input('Введите число: '))
+max_var_n = n + 1
+result = []
+if n > 9:
+    max_var_n = 10
+
+for i in range(1, max_var_n):
+    for k in range(1, max_var_n):
+        if k <= i:
+            continue
+
+        delitel = n % (i + k)
+        if delitel == 0:
+            result.append(i)
+            result.append(k)
+
+
+
+print(result)  # Вывод списком
+
+for i in range(len(result)):
+    result[i] = str(result[i])
+
+print(''.join(result))  #Вывод join строки из получившихся чисел
+
+for x in result:
+	print(x)  # Вывод поэлементно
+
+result = tuple(result)
+
+print(result)  # Вывод кортежем
+
+
---
+++ module_3_5_recursion.py
@@ -0,0 +1,4 @@
+def get_multipled_digits(number):
+	str_number = str(number)
+	
+	first = None
