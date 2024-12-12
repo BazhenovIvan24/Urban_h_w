c47e3ea8938614e225d6d19503bf22784ab8ef06
Ivan <144904628+BazhenovIvan24@users.noreply.github.com>
2024-12-12 23:20:49 +0300

    Update additional_task_module_1.py

--- additional_task_module_1.py
+++ additional_task_module_1.py
@@ -3,13 +3,13 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 average_marks = []
 i = 0
-sum_i = 0 #reset the sum of the marks
+
 #Создадим цикл, который будет записывать в список (list) average_marks - средние оценки студентов.
 while i < len(grades):
-    grades_i = (len(grades[i])) #length list of number of marks (student_i)
+    
      
     average_marks.append(sum(grades[i]) / len(grades[i]))  #append average marks to general list
-    sum_i = 0
+
     i += 1 # increase_i for cycle "while"
 
 
